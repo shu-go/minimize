de27a14b3a722068f6021759bb52f04e44291dd9
Shuhei Kubota <kubota.shuhei@gmail.com>
2017-08-27 18:24:52 +0900

    initial commit

---
+++ build.bat
@@ -0,0 +1,15 @@
+set NAME=minimize
+
+set GOOS=windows
+set GOARCH=386
+set ZIPNAME=%NAME%_%GOOS%_%GOARCH%.zip 
+go build -ldflags "-s -w" %*
+del %ZIPNAME% 2>nul
+zip %ZIPNAME% *.exe README.txt LICENSE.txt
+
+set GOOS=windows
+set GOARCH=amd64
+set ZIPNAME=%NAME%_%GOOS%_%GOARCH%.zip 
+rem go build -ldflags "-s -w" %*
+rem del %ZIPNAME% 2>nul
+rem zip %ZIPNAME% *.exe README.txt LICENSE.txt
