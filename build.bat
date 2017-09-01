b5aa60bd640b66b2f78791b241c3f11d5afed040
Shuhei Kubota <kubota.shuhei@gmail.com>
2017-09-01 23:02:46 +0900

    change build flags not to show a console window

--- build.bat
+++ build.bat
@@ -3,13 +3,13 @@ set NAME=minimize
 set GOOS=windows
 set GOARCH=386
 set ZIPNAME=%NAME%_%GOOS%_%GOARCH%.zip 
-go build -ldflags "-s -w" %*
+go build -ldflags "-s -w -H=windowsgui" %*
 del %ZIPNAME% 2>nul
 zip %ZIPNAME% *.exe README.txt LICENSE.txt
 
 set GOOS=windows
 set GOARCH=amd64
 set ZIPNAME=%NAME%_%GOOS%_%GOARCH%.zip 
-rem go build -ldflags "-s -w" %*
+rem go build -ldflags "-s -w -H=windowsgui" %*
 rem del %ZIPNAME% 2>nul
 rem zip %ZIPNAME% *.exe README.txt LICENSE.txt
